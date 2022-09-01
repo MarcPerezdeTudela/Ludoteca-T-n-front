4ef65a7d16767b0ffc1fbaf48e0655ac84577403
Marc Pérez de Tudela <mperezdetudelaaznar@gmail.com>
2022-09-01 17:10:52 +0200

    end 'Code: Listado filtrado' section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { CategoryModule } from './category/category.module';
 import { AuthorModule } from './author/author.module';
-
+import { GameModule } from './game/game.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -16,6 +16,7 @@ import { AuthorModule } from './author/author.module';
     CoreModule,
     CategoryModule,
     AuthorModule,
+    GameModule,
     BrowserAnimationsModule,
   ],
   providers: [],
